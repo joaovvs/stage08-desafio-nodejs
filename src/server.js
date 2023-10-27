27f39bd46d0bf5cf0ef4206aa9e2053ed893bcb1
Joao Vinicius <joaovvs@gmail.com>
2023-10-27 17:55:10 -0300

    install cors

--- src/server.js
+++ src/server.js
@@ -5,8 +5,9 @@ const routes = require('./routes');
 const connection = require("./database/knex");
 const uploadConfig = require("./configs/upload");
 
-
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
